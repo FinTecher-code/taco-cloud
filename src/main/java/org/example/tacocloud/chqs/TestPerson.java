a68f208019ce709dcaec1d768c9461b89419a432
jiaoxu <2356692289@qq.com>
2026-07-12 16:24:23 +0800

    chore: add chqs practice code

---
+++ src/main/java/org/example/tacocloud/chqs/TestPerson.java
@@ -0,0 +1,30 @@
+package org.example.tacocloud.chqs;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+class Person {
+    public String name;
+    private int age;
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    private void hello(){
+        System.out.println("Hello, " + this.name + "!");
+    }
+}
+
+public class TestPerson{
+    public static void main(String[] args) throws Exception{
+        Class<?> cls = Class.forName("Person");
+        Constructor<?> constructor = cls.getDeclaredConstructor(String.class, int.class);
+        constructor.setAccessible(true);
+        Object obj = constructor.newInstance("Alice",25);
+        Field field = cls.getDeclaredField("age");
+        field.setInt(obj, -1);
+        Method method = cls.getDeclaredMethod("hello");
+        method.invoke(obj);
+    }
+}
